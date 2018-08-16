5880738e67054d17fdeda4828732345f1510791c
Fachruzi Ramadhan <mabuak@live.com>
2018-08-16 12:41:16 +0700

    How Many Fingers

--- How Many Fingers/ViewController.swift
+++ How Many Fingers/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Properties
+    
+    @IBOutlet var fingersTextField: UITextField!
+    
+    @IBAction func guessButton(_ sender: Any) {
+        let diceRoll = String(arc4random_uniform(6))
+        
+        if( fingersTextField.text == diceRoll){
+            
+            resultLabel.text = "You're right!"
+            
+        } else {
+            
+            resultLabel.text = "Wrong! It was a " + diceRoll + "."
+            
+        }
+    }
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
